d830e40b21ab315a83a9558930683ffdac66d99a
adeard <adeardian1994@gmail.com>
2023-08-15 14:34:36 +0700

    trigger deploy

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
-	fmt.Println("Joss")
+	fmt.Println("Jos")
 }
 
 func handleRequests() {
